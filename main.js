cc3f753f01217593f0a425fcf3c51161cb014897
Mohammad-Moazzam <am78695pm@gmail.com>
2025-09-27 09:09:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+});
+
+window.addEventListener('scroll', () => {
+  navbar.classList.remove('active');
+  menu.classList.remove('bx-x');
+});
